1b82d48ba78229d7fbc9622cc25235d0a6691618
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2018-03-27 15:39:02 +0100

    Commented out currently invalid tests.
    See issue #17.

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -4,6 +4,7 @@ import Parser.Alex.Functions (runAlex)
 import qualified Parser.Parser as Parser
 import Parser.ErrorTypes (WrappedParserError)
 import Parser.Happy.Types (DesignFile)
+import Parser.Alex.BaseTypes (AlexPosn(..))
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -17,24 +18,33 @@ runParse str = runAlex str Parser.v1987
 -- |All parser tests
 tests :: TestTree
 tests = testGroup "Parser Tests"
-   [ justContext
-   , doubleJustContext
-   , emptyLibs
+   --[ justContext
+   --, doubleJustContext
+   [ emptyLibs
    ]
 
--- |Only library clauses
-justContext :: TestTree
-justContext = testGroup "Design file with just context clauses"
-   [ justLibraryClause
-   , basicUseClauses
-   ]
-
--- |Test for multiple design units with only library clauses
-doubleJustContext :: TestTree
-doubleJustContext = testGroup "Design file with multiple sets of context clauses"
-   [ twoLibraryClauses
-   , libraryAndUseClauseMix
-   ]
+---- |Only library clauses
+--justContext :: TestTree
+--justContext = testGroup "Design file with just context clauses"
+--   [ justLibraryClause
+--   , basicUseClauses
+--   ]
+--
+---- |Only a library statement
+--justLibraryClause :: TestTree
+--justLibraryClause = testGroup "Design file with a single library statement"
+--   [ testCase "Standard library \"library std;\"" $
+--      runParse "library std;" @?= Right DesignFile
+--         [PosnWrapper { getPos = AlexPn 0 1 0, unPos =
+--            DesignUnit
+--               
+--
+---- |Test for multiple design units with only library clauses
+--doubleJustContext :: TestTree
+--doubleJustContext = testGroup "Design file with multiple sets of context clauses"
+--   [ twoLibraryClauses
+--   , libraryAndUseClauseMix
+--   ]
 
 -- |Basic library unit tests (library units empty)
 -- Shell lib units IE no contents
