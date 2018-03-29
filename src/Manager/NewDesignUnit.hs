cfbb59f4b861c2d21f1ce6d937d3912141247374
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2018-03-29 16:52:26 +0100

    First compiling manager file.
    Types are correct, but no contents (other than initial state setup).

--- src/Manager/NewDesignUnit.hs
+++ src/Manager/NewDesignUnit.hs
@@ -1,16 +1,28 @@
 module Manager.NewDesignUnit where
 
+import Control.Monad.Trans.State
+
 import qualified Manager.Args as Args (Options)
+import qualified Netlister.Builtin.Netlist as InitialNetlist (netlist)
+import Netlister.TypeData (NetlistStore)
+import qualified Parser.Parser as Parser (v1987)
+import Parser.ErrorTypes (WrappedParserError)
+import Parser.Happy.Types (DesignFile)
+import Parser.Alex.Functions (runAlex)
 
-createTop :: Args.Options -> IO ()
+createTop :: Args.Options -> StateT NetlistStore IO ()
 createTop options = do
+   put InitialNetlist.netlist
 -- Call create function for topmost file
+--
+-- Need state monad of current scoped and already parsed objects to be setup?
+-- Need to use StateM or whatever to combine state with Either monad?
 
-create :: String -> IO ()
+--create :: String -> StateT NetlistStore IO
 -- Confirm file name
 -- Parse file
 -- Check dependencies
 -- Parse new files
 
-parse :: String -> Either WrapperParserError DesignFile
+parse :: String -> Either WrappedParserError DesignFile
 parse s = runAlex s Parser.v1987
